4fb0bed4eb57ba465103e53f3bd6b6d79073dc9c
dung <11230522@st.neu.edu.vn>
2026-05-10 16:18:56 +0700

    chore: stop tracking create.py in notebooks

--- notebooks/run_eda.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-print("Creating base EDA notebook...")
-subprocess.run(["python", "notebooks/create_eda.py"], check=True)
-
-print("Appending extra sections...")
-subprocess.run(["python", "notebooks/create_eda_extra.py"], check=True)
-
-print("Done.")
